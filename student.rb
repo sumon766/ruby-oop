7cb8ee71106215a7be47c09bc395d0ae5abf7b9f
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-06 20:48:39 +0600

    Fix rubocop errors

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 class Student < Person
-    attr_accessor :classroom
+  attr_accessor :classroom
 
-    def initialize(id, age, classroom, name = "Unknown", parent_permission = true)
-      super(id, age, name, parent_permission)
-      @classroom = classroom
-    end
-  
-    def play_hooky
-      "¯\\(ツ)/¯"
-    end
+  def initialize(id, age, classroom, name: 'Unknown', parent_permission: true)
+    super(id, age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
 end
