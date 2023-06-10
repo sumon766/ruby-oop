92ffef2ed2f6c6194a50f059f03cdc161f9bf0f5
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-10 21:14:59 +0600

    Add interface and fix linter errors

--- main.rb
+++ main.rb
@@ -1,32 +1,36 @@
 require_relative 'app'
 
+def display_menu(app)
+  app.options
+  print 'Enter your choice: '
+  gets.chomp.to_i
+end
+
+def handle_choice(app, choice)
+  actions = {
+    1 => -> { app.list_books },
+    2 => -> { app.list_people },
+    3 => -> { app.create_person },
+    4 => -> { app.create_book },
+    5 => -> { app.create_rental },
+    6 => -> { app.list_rentals_for_person },
+    7 => -> { puts 'Exiting the app...' },
+    default: -> { puts 'Invalid choice. Please try again.' }
+  }
+
+  action = actions[choice] || actions[:default]
+  action.call
+
+  choice == 7
+end
+
 def main
   app = App.new
 
   loop do
-    app.options
-    print "Enter your choice: "
-    choice = gets.chomp.to_i
-
-    case choice
-    when 1
-      app.list_books
-    when 2
-      app.list_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.list_rentals_for_person
-    when 7
-      puts "Exiting the app..."
-      break
-    else
-      puts "Invalid choice. Please try again."
-    end
+    choice = display_menu(app)
+    exit_app = handle_choice(app, choice)
+    break if exit_app
 
     puts "\n"
   end
